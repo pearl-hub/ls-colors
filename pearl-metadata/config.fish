fbce4d2d7754c5f080da467133c411ce4b8a509d
fsquillace <feel.sqoox@gmail.com>
2016-05-23 19:50:38 +0100

    Use pearl_attempt_command to run dircolors

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,3 +1,3 @@
-eval (dircolors -b $PEARL_PKGDIR/module/LS_COLORS | sed -e '2d' -e 's/^LS_/set LS_/' -e 's/=/ /' )
+eval (pearl_attempt_command dircolors -b $PEARL_PKGDIR/module/LS_COLORS | sed -e '2d' -e 's/^LS_/set LS_/' -e 's/=/ /' )
 
 # vim: ft=fish
