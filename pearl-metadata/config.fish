ea080f87f0511d5737a0e15b251507c4d4b48e1e
Filippo Squillace <squillace.filippo@gmail.com>
2018-02-04 11:46:26 +1100

    Add buava dependency

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,3 +1,5 @@
+source $PEARL_PKGDIR/buava/lib/osx-compat.fish
+
 eval (osx_attempt_command dircolors -b $PEARL_PKGDIR/module/LS_COLORS | sed -e '2d' -e 's/^LS_/set LS_/' -e 's/=/ /' )
 
 # vim: ft=fish
--- pearl-metadata/config.sh
+++ pearl-metadata/config.sh
@@ -1,3 +1,5 @@
+source $PEARL_PKGDIR/buava/lib/osx-compat.sh
+
 eval $(osx_attempt_command dircolors -b $PEARL_PKGDIR/module/LS_COLORS)
 
 # vim: ft=sh
